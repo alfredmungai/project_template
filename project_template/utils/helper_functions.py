fad1d5b5130b75b43af45799ad9a0859620602e7
Alfred Mungai Ndungu <alfred.mungai@gmail.com>
2021-04-01 06:40:01 -0500

    Update helper_functions.py

--- project_template/utils/helper_functions.py
+++ project_template/utils/helper_functions.py
@@ -17,7 +17,12 @@ Helper functions.
 """
 
 import numpy as np
+import logging
+from time import time
 
+import yaml
+
+logger = logging.getLogger(__name__)
 
 def power_up(use_number, use_power):
     """
@@ -25,3 +30,27 @@ def power_up(use_number, use_power):
     """
 
     return np.power(use_number, use_power)
+
+def get_yaml_content(use_path):
+    """
+    Read file in YAML format.
+    """
+    with open(use_path, "r") as file_stream:
+        yaml_content = yaml.safe_load(file_stream)
+
+    return yaml_content
+
+def print_execution_time(function):
+    """
+    Measuring running time.
+    """
+
+    def timed(*args, **kw):
+        time_start = time()
+        return_value = function(*args, **kw)
+        time_end = time()
+        execution_time = time_end - time_start
+        logger.info("`%s` ran in %.2f seconds.", function.__name__, execution_time)
+        return return_value
+
+    return timed
